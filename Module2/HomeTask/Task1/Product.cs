40605a95a6d575dee187ae60768997f789b8812a
mrcdevv <marcoemorales2015@gmail.com>
2024-10-02 23:26:08 -0300

    Module2 - Home Task completed

---
+++ Module2/HomeTask/Task1/Product.cs
@@ -0,0 +1,28 @@
+﻿namespace Task1
+{
+    public class Product
+    {
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public string Name { get; set; }
+
+        public double Price { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Product prod)
+            {
+                if (prod.Name == Name && prod.Price == Price)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
