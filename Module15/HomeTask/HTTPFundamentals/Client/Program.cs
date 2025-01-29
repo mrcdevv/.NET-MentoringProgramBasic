10291d3e9f7feb1d07dbde3337ab86d2c466f31f
mrcdevv <marcoemorales2015@gmail.com>
2025-01-29 02:36:59 -0300

    Module15 - Home Task completed

---
+++ Module15/HomeTask/HTTPFundamentals/Client/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static readonly HttpClient client = new HttpClient();
+
+    static async Task Main(string[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Select a task:");
+            Console.WriteLine("1. Get my name");
+            Console.WriteLine("2. Test HTTP status codes");
+            Console.WriteLine("3. Get my name from header");
+            Console.WriteLine("4. Get my name from cookies");
+            Console.WriteLine("5. Exit");
+            Console.Write("Option: ");
+
+            string option = Console.ReadLine();
+
+            switch (option)
+            {
+                case "1":
+                    await GetMyName();
+                    break;
+                case "2":
+                    await TestHttpStatusCodes();
+                    break;
+                case "3":
+                    await GetMyNameFromHeader();
+                    break;
+                case "4":
+                    await GetMyNameFromCookies();
+                    break;
+                case "5":
+                    return;
+                default:
+                    Console.WriteLine("Invalid option.");
+                    break;
+            }
+        }
+    }
+
+    private static async Task GetMyName()
+    {
+        string url = "http://localhost:8888/MyName/";
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            string myName = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"My Name: {myName}");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in GetMyName: {ex.Message}");
+        }
+    }
+
+    private static async Task TestHttpStatusCodes()
+    {
+        string[] urls = {
+            "http://localhost:8888/Information/",
+            "http://localhost:8888/Success/",
+            "http://localhost:8888/Redirection/",
+            "http://localhost:8888/ClientError/",
+            "http://localhost:8888/ServerError/"
+        };
+
+        foreach (var url in urls)
+        {
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(url);
+                Console.WriteLine($"URL: {url}, Status Code: {response.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in TestHttpStatusCodes for URL {url}: {ex.Message}");
+            }
+        }
+    }
+
+    private static async Task GetMyNameFromHeader()
+    {
+        string url = "http://localhost:8888/MyNameByHeader/";
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.Headers.TryGetValues("X-MyName", out var values))
+            {
+                Console.WriteLine($"My Name from Header: {string.Join(", ", values)}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in GetMyNameFromHeader: {ex.Message}");
+        }
+    }
+
+    private static async Task GetMyNameFromCookies()
+    {
+        string url = "http://localhost:8888/MyNameByCookies/";
+        try
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            if (response.Headers.TryGetValues("Set-Cookie", out var values))
+            {
+                foreach (var cookie in values)
+                {
+                    if (cookie.Contains("MyName"))
+                    {
+                        Console.WriteLine($"My Name from Cookies: {cookie.Split('=')[1]}");
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in GetMyNameFromCookies: {ex.Message}");
+        }
+    }
+}
---
+++ Module15/HomeTask/HTTPFundamentals/Listener/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    private static HttpListener listener;
+    private static bool running = true;
+
+    static async Task Main(string[] args)
+    {
+        listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:8888/");
+        listener.Start();
+        Console.WriteLine("Listener started...");
+
+        while (running)
+        {
+            try
+            {
+                HttpListenerContext context = await listener.GetContextAsync();
+                Console.WriteLine($"Request received: {context.Request.Url}");
+                await ProcessRequestAsync(context);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error in Listener: {ex.Message}");
+            }
+        }
+
+        listener.Stop();
+    }
+
+    private static async Task ProcessRequestAsync(HttpListenerContext context)
+    {
+        HttpListenerRequest request = context.Request;
+        HttpListenerResponse response = context.Response;
+
+        string resourcePath = request.Url.Segments[1].TrimEnd('/');
+
+        Console.WriteLine($"Processing resource: {resourcePath}");
+
+        switch (resourcePath)
+        {
+            case "MyName":
+                await GetMyName(response);
+                break;
+            case "Information":
+                await SendStatusAsync(response, HttpStatusCode.OK); // This first one isn't working if I put a Information status code. I dont know how make it work
+                break;
+            case "Success":
+                await SendStatusAsync(response, HttpStatusCode.OK);
+                break;
+            case "Redirection":
+                await SendStatusAsync(response, HttpStatusCode.Redirect);
+                break;
+            case "ClientError":
+                await SendStatusAsync(response, HttpStatusCode.BadRequest);
+                break;
+            case "ServerError":
+                await SendStatusAsync(response, HttpStatusCode.InternalServerError);
+                break;
+            case "MyNameByHeader":
+                await GetMyNameByHeader(response);
+                break;
+            case "MyNameByCookies":
+                await GetMyNameByCookies(response);
+                break;
+            case "Exit":
+                running = false;
+                response.StatusCode = (int)HttpStatusCode.OK;
+                await response.OutputStream.WriteAsync(Encoding.UTF8.GetBytes("Server stopped."), 0, 0);
+                break;
+            default:
+                response.StatusCode = (int)HttpStatusCode.NotFound;
+                string notFoundMessage = "Resource not found.";
+                byte[] notFoundBuffer = Encoding.UTF8.GetBytes(notFoundMessage);
+                response.ContentLength64 = notFoundBuffer.Length;
+                await response.OutputStream.WriteAsync(notFoundBuffer, 0, notFoundBuffer.Length);
+                break;
+        }
+
+        response.Close();
+    }
+
+    private static async Task GetMyName(HttpListenerResponse response)
+    {
+        string myName = "Marco";
+        byte[] buffer = Encoding.UTF8.GetBytes(myName);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+    }
+
+    private static async Task SendStatusAsync(HttpListenerResponse response, HttpStatusCode statusCode)
+    {
+        response.StatusCode = (int)statusCode;
+        string statusMessage = $"Status Code: {statusCode}";
+        byte[] buffer = Encoding.UTF8.GetBytes(statusMessage);
+        response.ContentLength64 = buffer.Length;
+        await response.OutputStream.WriteAsync(buffer, 0, buffer.Length);
+    }
+
+    private static async Task GetMyNameByHeader(HttpListenerResponse response)
+    {
+        response.Headers.Add("X-MyName", "Marco");
+        response.StatusCode = (int)HttpStatusCode.OK;
+        await Task.CompletedTask;
+    }
+
+    private static async Task GetMyNameByCookies(HttpListenerResponse response)
+    {
+        response.Cookies.Add(new Cookie("MyName", "Marco"));
+        response.StatusCode = (int)HttpStatusCode.OK;
+        await Task.CompletedTask;
+    }
+}
